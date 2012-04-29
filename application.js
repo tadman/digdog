03b992622186b26a875e44e8dea6789df20f1b18
Scott Tadman <github@tadman.ca>
2012-04-29 17:42:38 -0400

    Adding frame counter, simple run-loop

--- application.js
+++ application.js
@@ -1,11 +1,52 @@
+// -- Game Logic ------------------------------------------------------------
+
+$.digdog = {
+  run: function() {
+    $.renderHelper.loop(function(frame) {
+      $.digdog.before_render();
+      $.digdog.render();
+      $.digdog.after_render();
+    });
+  },
+  before_render: function() {
+  },
+  render: function() {
+  },
+  after_render: function() {
+    $('#frame').html($.renderHelper.frame);
+  }
+};
+
+$.renderHelper = {
+  frame: 0,
+  start: Date.now(),
+  requestAnimFrame: (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  ),
+  loop: function(proc) {
+    var do_loop = function() {
+      proc($.renderHelper.frame);
+      $.renderHelper.requestAnimFrame(do_loop);
+      $.renderHelper.frame++;
+    }
+    
+    do_loop();
+  }
+};
+
 // -- Page Ready ------------------------------------------------------------
 
 $(function() {
   // This function is run when the page is fully loaded and ready for
   // scripting.
   $('#status').html('READY');
+  
+  $.digdog.run();
 });
-
-// -- Game Logic ------------------------------------------------------------
-
-// ...
