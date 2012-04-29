256bcf0a87504244b02e330545b3d6bcdd35e2e3
Scott Tadman <github@tadman.ca>
2012-04-29 17:19:21 -0400

    Adding skeleton page

---
+++ application.js
@@ -0,0 +1,11 @@
+// -- Page Ready ------------------------------------------------------------
+
+$(function() {
+  // This function is run when the page is fully loaded and ready for
+  // scripting.
+  $('#status').html('READY');
+});
+
+// -- Game Logic ------------------------------------------------------------
+
+// ...
